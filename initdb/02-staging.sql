994d2fb603c724fec57c4a93961ad7f1faeeec84
QuentinThuler <quentin.thuler@heig-vd.ch>
2026-03-12 15:21:27 +0100

    Scripts d'initialisation dans docker-compose.yml

--- initdb/02-staging.sql
+++ initdb/02-staging.sql
@@ -1,27 +1,4 @@
 -- Active: 1772721152986@@127.0.0.1@5432@infradon
-CREATE SCHEMA IF NOT EXISTS staging;
-
-CREATE TABLE staging.inventaire_mobilier (
-    id TEXT, type TEXT, materiau TEXT, lieu TEXT,
-    latitude TEXT, longitude TEXT,
-    date_installation TEXT, etat TEXT, remarques TEXT
-);
-
-CREATE TABLE staging.signalements (
-    date TEXT, signale_par TEXT, objet TEXT,
-    description TEXT, urgence TEXT, statut TEXT
-);
-
-CREATE TABLE staging.interventions (
-    date TEXT, objet TEXT, type_intervention TEXT,
-    technicien TEXT, duree TEXT, cout_materiel TEXT, remarques TEXT
-);
-
-CREATE TABLE staging.fournisseurs (
-    entreprise TEXT, contact TEXT, telephone TEXT,
-    email TEXT, type_materiel TEXT, remarques TEXT
-);
-
 COPY staging.inventaire_mobilier 
 FROM '/docker-data/inventaire_mobilier.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
