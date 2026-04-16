3d99839363eb4e1506150728da40c14a2bb13618
QuentinThuler <quentin.thuler@heig-vd.ch>
2026-04-16 15:23:41 +0200

    fix

--- seeds/insertDataScripte/inventaire-insert.sql
+++ seeds/insertDataScripte/inventaire-insert.sql
@@ -23,3 +23,37 @@ INSERT INTO type_etats (nom)
 SELECT DISTINCT etat 
 FROM stg_inventaire
 WHERE etat IS NOT NULL;
+
+
+-- TO DO Insert mobilier
+INSERT INTO mobiliers (
+    latitude,
+    longitude,
+    date_installation,
+    remarque,
+    id_type_mobilier,
+    id_type_lieu,
+    id_type_materiaux,
+    id_type_etat
+)
+SELECT
+    REPLACE(s.latitude, ',', '.')::FLOAT,
+    REPLACE(s.longitude, ',', '.')::FLOAT,
+    s.date_installation::DATE,
+    s.remarques,
+    tm.id,
+    tl.id,
+    tmat.id,
+    te.id
+FROM stg_inventaire s
+-- Résolution FK type mobilier
+JOIN type_mobiliers tm   ON LOWER(TRIM(tm.nom)) = LOWER(TRIM(s.type))
+-- Résolution FK lieu
+JOIN type_lieux tl       ON LOWER(TRIM(tl.nom)) = LOWER(TRIM(s.lieu))
+-- Résolution FK matériaux
+JOIN type_materiaux tmat ON LOWER(TRIM(tmat.nom)) = LOWER(TRIM(s.materiau))
+-- Résolution FK état
+JOIN type_etats te       ON LOWER(TRIM(te.nom)) = LOWER(TRIM(s.etat));
+
+
+SELECT * from mobiliers;
