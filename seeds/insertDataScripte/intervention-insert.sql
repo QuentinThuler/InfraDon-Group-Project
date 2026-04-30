9943a40dc6121c7b3dba74bc50acf3b1b92561e0
QuentinThuler <quentin.thuler@heig-vd.ch>
2026-04-30 14:59:13 +0200

    Intervention script fixes

--- seeds/insertDataScripte/intervention-insert.sql
+++ seeds/insertDataScripte/intervention-insert.sql
@@ -1,8 +1,8 @@
 SELECT * from stg_intervention;
 
 -- insert data into table techniciens from stg_intervention
-INSERT INTO techniciens (nom, prenom)
-SELECT DISTINCT SPLIT_PART(technicien, ' ', 1) AS nom, SPLIT_PART(technicien, ' ', 2) AS prenom  
+INSERT INTO techniciens (nom, prenom, description)
+SELECT DISTINCT SPLIT_PART(technicien, ' ', 1) AS nom, SPLIT_PART(technicien, ' ', 2) AS prenom, '' AS description
 FROM stg_intervention;
 
 -- insert data into table type_interventions from stg_intervention
@@ -18,22 +18,23 @@ INSERT INTO interventions (
     remarque,
     id_technicien,
     id_mobilier,
-    id_type_intervention,
+    id_type_intervention
 )
 SELECT
     s.date::DATE,
-    REPLACE(s.duree, ',', '.')::FLOAT
-    s.cout_materiel::NUMBER,
+    REPLACE(s.duree, ',', '.')::FLOAT8,
+    s.cout_materiel::NUMERIC,
     s.remarques,
-    tl.id,
-    tmat.id,
-    te.id
+    t.id,
+    m.id,
+    ti.id
 FROM stg_intervention s
 -- Résolution FK technicien
-JOIN techniciens t ON LOWER(TRIM(t.nom)) = LOWER(SPLIT_PART(s.technicien, ' ', 1) AS nom)
+JOIN techniciens t ON LOWER(TRIM(t.nom)) = LOWER(SPLIT_PART(s.technicien, ' ', 1))
 -- Résolution FK type_intervention
-JOIN type_intervations ti ON LOWER(TRIM(ti.nom)) = LOWER(TRIM(s.type_intervention))
+JOIN type_interventions ti ON LOWER(TRIM(ti.nom)) = LOWER(TRIM(s.type_intervention))
+JOIN mobiliers m ON LOWER(TRIM(ti.nom)) = LOWER(TRIM(s.type_intervention));
 
 -- TO DO Résolution FK mobilier join mobilier on ? -> type_mobilier (banc), -> type_Lieux (route de lausanne)
-JOIN mobiliers m ON LOWER(TRIM(ti.nom)) = LOWER(TRIM(s.type_intervention));
+
 
